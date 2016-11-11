f3cbf9906f01605ab08189e7c2357930a3041a86
wapalxj <wapalxj@163.com>
2016-11-11 21:08:36 +0800

    16.11.11

--- C3_3_ConentProvider/c3_02_mycontentprovider/src/main/java/com/c3/vero/c3_02_mycontentprovider/MainActivity.java
+++ C3_3_ConentProvider/c3_02_mycontentprovider/src/main/java/com/c3/vero/c3_02_mycontentprovider/MainActivity.java
@@ -61,27 +61,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         return cursor;
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 
     //用于存储cursor指向记录的id
     String cur_id=null;
